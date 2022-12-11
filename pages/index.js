46322417fd2e822441efa130cedd8d64f7ec59c0
Aryan Singh <singh.aryan837@gmail.com>
2022-12-11 19:37:54 +0530

    Linear search

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { Flex,Box,Text,Button } from '@chakra-ui/react'
-
+import {baseUrl,fetchApi} from '../utils/fetchApi'
 
 export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
@@ -17,7 +17,8 @@ export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, link
   </Flex>
 );
 
-export default function Home() {
+export default function Home({propertiesForRent,propertiesForSale}) {
+  console.log(propertiesForRent,propertiesForSale);
   return (
     <Box>
       
@@ -50,4 +51,15 @@ export default function Home() {
      
     </Box>
   )
+}
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    },
+  };
 }
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
-const baseUrl='https://bayut.p.rapidapi.com'
+export const baseUrl='https://bayut.p.rapidapi.com'
 // headers: {
 //     'X-RapidAPI-Key': '79114d0e2bmshfadaa4f8280be44p17f9bcjsnef4a89a93412',
 //     'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
 //   }
 
 export const fetchApi=async (url)=>{
-    const response=await axios.get((url),{
-        
+    const {data}=await axios.get((url),{
+        headers: {
+                'X-RapidAPI-Key': '79114d0e2bmshfadaa4f8280be44p17f9bcjsnef4a89a93412',
+                'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+              }
+
     })
+    return data;
 }
